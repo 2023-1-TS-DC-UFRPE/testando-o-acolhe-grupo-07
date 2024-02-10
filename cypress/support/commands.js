1bdb634026357c6664f01e2ec94804e962147125
notebook-accioly <acciolyluan@gmail.com>
2024-02-10 16:18:31 -0300

    refactor: change import statements in commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,15 +1,12 @@
-const { Dashboard } = require("../e2e/pages/dashboard");
-const { Home } = require("../e2e/pages/home");
-const { Login } = require("../e2e/pages/login");
+import { Home } from "../e2e/pages/home";
+import { Login } from "../e2e/pages/login";
 
 const home = new Home();
 const login = new Login();
-const dashboard = new Dashboard();
 
 Cypress.Commands.add("login", (email, password) => {
   home.loginButton().click();
   login.emailInput().type(email);
   login.passwordInput().type(password);
   login.submitButton().click();
-  dashboard.title().should("contain.text", "Painel de Visualização");
 });
