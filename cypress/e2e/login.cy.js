5edac662f916f7602dae93d92f422232a91b9399
notebook-accioly <acciolyluan@gmail.com>
2024-02-10 16:19:39 -0300

    test: add DOC-02

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,24 @@
-describe("Testes de Login", () => {
+import { Dashboard } from "./pages/dashboard";
+import { Login } from "./pages/login";
+const dashboard = new Dashboard();
+const login = new Login();
+const data = {
+  email: Cypress.env("CYPRESS_EMAIL"),
+  password: Cypress.env("CYPRESS_PASSWORD"),
+  invalidEmail: "invalidemail@invalid.com",
+  invalidPassword: "invalidPassword",
+};
+
+describe("EP01", () => {
   it("Login feito com sucesso", () => {
     cy.visit("/");
-    cy.login(Cypress.env("CYPRESS_EMAIL"), Cypress.env("CYPRESS_PASSWORD"));
+    cy.login(data.email, data.password);
+    dashboard.title().should("contain.text", "Painel de Visualização");
+  });
+
+  it("DOC-02 - Tentativa de logar com senha errada", () => {
+    cy.visit("/");
+    cy.login(data.email, data.invalidPassword);
+    login.failLoginAlert().should("be.visible");
   });
 });
