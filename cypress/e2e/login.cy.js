9c226026ae9e569322103ab5bd2b5cceb246d06d
Luan Accioly <acciolyluan@gmail.com>
2024-02-10 00:58:26 -0300

    test: add login test case

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,5 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("Testes de Login", () => {
+  it("Login feito com sucesso", () => {
     cy.visit("/");
     cy.login(Cypress.env("email"), Cypress.env("password"));
   });
