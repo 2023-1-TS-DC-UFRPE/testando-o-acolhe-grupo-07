cb75f8629ee4a5d62ac649117e9150bf87b48e57
Luan Accioly <acciolyluan@gmail.com>
2024-02-17 17:56:32 -0300

    test(POM): add member functionality and update birth date selection

--- cypress/e2e/pages/shelters/received/member.js
+++ cypress/e2e/pages/shelters/received/member.js
@@ -12,6 +12,14 @@ export class Member {
     cy.get("#colorRace").click();
     cy.get("mat-option").contains(color).click();
   }
+  cpf() {
+    return cy.get("#cpf");
+  }
+
+  selectRelationship(relationship) {
+    cy.get("#relationshipWithChief").click();
+    cy.get("mat-option").contains(relationship).click();
+  }
 
   /**
    * @param {boolean} isResponsible
@@ -22,7 +30,31 @@ export class Member {
       : cy.log("Não é responsável");
   }
 
-  birthDate() {
-    cy.get('[formcontrolname="birthdate"]');
+  selectBirthDate(date) {
+    cy.contains("Data de Nascimento").click();
+    cy.get(".mat-calendar-content").contains(date.year).click();
+    cy.get(".mat-calendar-content").contains(date.month).click();
+    cy.get(".mat-calendar-content").contains(date.day).click();
+  }
+
+  addButton() {
+    return cy.get("button").contains("Adicionar");
+  }
+
+  cancelButton() {
+    return cy.get(".dialog__close");
+  }
+
+  addMember(member) {
+    this.name().type(member.name);
+    this.cpf().type(member.cpf);
+    this.selectGender(member.gender);
+    this.selectBirthDate(member.birthDate);
+    this.declareColor(member.declaredColor);
+    member.isResponsible
+      ? this.isResponsible(member.isResponsible)
+      : this.selectRelationship(member.relationshipToResponsible);
+
+    this.addButton().click();
   }
 }
