4df07a3164cd1d06f4a1d7fb85190b817d28e43d
Luan Accioly <acciolyluan@gmail.com>
2024-02-17 01:36:19 -0300

    test(data): add ep03 fixture

---
+++ cypress/fixtures/ep03Data.js
@@ -0,0 +1,34 @@
+export const receivedData = {
+  zipCode: "50721540",
+  number: 61,
+  affectedHouse: "Sim, está habitável e não precisa de reparos",
+  members: [
+    {
+      name: "Luan Accioly",
+      gender: "Masculino",
+      birthDate: "2000-12-27",
+      declaredColor: "Parda",
+      isResponsible: true,
+      relationshipToResponsible: "",
+      isDeficient: false,
+    },
+    {
+      name: "Átila Rodrigues",
+      gender: "Masculino",
+      birthDate: "27/12/2000",
+      declaredColor: "Parda",
+      isResponsible: false,
+      relationshipToResponsible: "Filho(a)",
+      isDeficient: false,
+    },
+    {
+      name: "Mario Leandro",
+      gender: "Não Informado",
+      birthDate: "27/12/2000",
+      declaredColor: "Parda",
+      isResponsible: false,
+      relationshipToResponsible: "Filho(a)",
+      isDeficient: false,
+    },
+  ],
+};
