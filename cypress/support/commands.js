dbd1bd18a66b0ef6c355390409dc00964202109a
Mario Leandro <marioleandro34@gmail.com>
2024-02-22 17:44:26 -0300

    test: add createShelter command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,3 +10,32 @@ Cypress.Commands.add("login", (email, password) => {
   login.passwordInput().type(password);
   login.submitButton().click();
 });
+
+Cypress.Commands.add("createShelter", (data) => {
+  const getToken = () => {
+    const options = {
+      method: 'POST',
+      url: "https://acolhebe-disciplina.innovagovlab.org/api/v1/auth/login",
+      body: {
+        "password": Cypress.env("PASSWORD"),
+        "username": Cypress.env("EMAIL"), 
+      }
+    };
+    return cy.request(options)
+  }
+  
+  getToken().then(response => {
+    const options = {
+      method: 'POST',
+      url: "https://acolhebe-disciplina.innovagovlab.org/api/v1/shelter",
+      body: data,
+      headers: {
+        "X-Auth-Token": response.body,
+        "Content-Type": "application/json"
+      }
+    };
+    return cy.request(options)
+  })
+
+});
+
