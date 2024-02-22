b32bb4fad78dcc44c90470e0dd6a8564f8ce0cdb
Mario Leandro <marioleandro34@gmail.com>
2024-02-22 16:18:03 -0300

    test: add DOC-04

--- cypress/e2e/ep06.cy.js
+++ cypress/e2e/ep06.cy.js
@@ -1,13 +1,26 @@
 import { Dashboard } from "./pages/dashboard";
 import { ReceivedPage } from "./pages/received";
+import { Sidebar } from "./pages/sidebar";
 
 const dashboard = new Dashboard();
 const received = new ReceivedPage();
+const sidebar = new Sidebar();
+
 describe("ST-05", () => {
   beforeEach(() => {
     cy.visit("/");
     cy.login(Cypress.env("EMAIL"), Cypress.env("PASSWORD"));
     cy.contains("Painel de Visualização").should("be.visible");
+  });
+
+
+  it("DOC-04", () => {
+    cy.intercept('GET', 'https://acolhebe-disciplina.innovagovlab.org/api/v1/family?personSituation=pcd&query=&page=0&size=25').as('searchFiltered')
+    sidebar.acolhidosButton().click();
+    received.receivedFilter().click();
+    received.chooseReceivedFilter('PCDs').click()
+    cy.wait('@searchFiltered').its('response.statusCode').should('eq', 200)
+
   });
 
   it("DOC-09", () => {
